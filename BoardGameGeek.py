c557f8b12f38614b1dece157f417ce740b4ea9fb
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-04-25 13:47:34 -0700

    skip and notify user when game not found

--- BoardGameGeek.py
+++ BoardGameGeek.py
@@ -1,36 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import csv
 from pathlib import Path
 
 game_list = input('Enter games seperated by "|" >>')
 game_list = (game_list .split('|'))
 browser=webdriver.Chrome()
+
 for game in game_list:
     browser.get('https://boardgamegeek.com/')
     search_bar = browser.find_element(By.NAME, 'searchTerm')
     search_bar.send_keys(game)
     search_bar.send_keys(Keys.ENTER)
-    first_result = browser.find_element(By.CLASS_NAME, 'primary')
-    game_name = browser.find_element(By.CLASS_NAME, 'primary')
-    game_rank = browser.find_element(By.CLASS_NAME, 'collection_rank')
-    game_name = game_name.text
-    game_rank = game_rank.text
-    first_result.click()
-    game_URL = browser.current_url
-    player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-primary')
-    recomended_player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-secondary')
-    player_count=player_count.text
-    recomended_player_count=recomended_player_count.text
-    csv_file = Path('BoardGameGeek Game Data.csv')
-    if csv_file.exists():
-        with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
-            writer = csv.writer(save)
-            writer.writerow([game_name, game_rank, player_count, recomended_player_count, game_URL])
-    else:
-        with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
-            writer = csv.writer(save)
-            writer.writerow(['Game Name', 'Rank','Player Count','Recommended Player Count', 'BGG URL'])                
-            writer.writerow([game_name, game_rank, player_count, recomended_player_count, game_URL])
+    try:
+        first_result = browser.find_element(By.CLASS_NAME, 'primary')
+        game_name = browser.find_element(By.CLASS_NAME, 'primary')
+        game_rank = browser.find_element(By.CLASS_NAME, 'collection_rank')
+        game_name = game_name.text
+        game_rank = game_rank.text
+        first_result.click()
+        game_URL = browser.current_url
+        player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-primary')
+        recomended_player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-secondary')
+        player_count=player_count.text
+        recomended_player_count=recomended_player_count.text
+        csv_file = Path('BoardGameGeek Game Data.csv')
+        if csv_file.exists():
+            with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
+                writer = csv.writer(save)
+                writer.writerow([game_name, game_rank, player_count, recomended_player_count, game_URL])
+        else:
+            with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
+                writer = csv.writer(save)
+                writer.writerow(['Game Name', 'Rank','Player Count','Recommended Player Count', 'BGG URL'])                
+                writer.writerow([game_name, game_rank, player_count, recomended_player_count, game_URL])
+    except NoSuchElementException:
+         print(game + ' not found, skipped')
+
 browser.quit
