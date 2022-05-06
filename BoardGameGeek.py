28a6ecdc30dbaeb23a1b0b776175c5e4f0aa2bc4
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-06 15:35:48 -0700

    put scraping code in def

--- BoardGameGeek.py
+++ BoardGameGeek.py
@@ -8,6 +8,47 @@ from pathlib import Path
 def write_fields(x):
     x.writerow([game_name, type, game_rating, game_rank, weight, play_time, player_count, recomended_player_count, game_URL])
 
+def add_to_csv():
+    csv_file = Path('BoardGameGeek Game Data.csv')
+    if csv_file.exists():
+        with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
+            writer = csv.writer(save)
+            write_fields(writer)
+    else:
+        with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
+            writer = csv.writer(save)
+            writer.writerow(['Game Name', 'Type', 'Rating', 'Rank', 'Weight', 'Play Time', 'Player Count','Recommended Player Count', 'BGG URL'])               
+            write_fields(writer)
+
+def scrape_game_page():
+    global game_name, type, game_rating, game_rank, weight, play_time, player_count, recomended_player_count, game_URL
+    game_URL = browser.current_url
+    type = 'N/A'
+    if str(game_URL).find('https://boardgamegeek.com/boardgame/') != -1:
+        type = "Game"
+    if str(game_URL).find('https://boardgamegeek.com/boardgameexpansion/') != -1:
+        type = "Expansion"
+    game_name = browser.find_element(By.CSS_SELECTOR, 'h1>a[class="ng-binding"]')
+    try:
+        game_rank = browser.find_element(By.CSS_SELECTOR, 'a[class="rank-value ng-binding ng-scope"]')
+        game_rank = game_rank.text
+    except NoSuchElementException:
+        game_rank = 'N/A'
+    game_rating = browser.find_element(By.CSS_SELECTOR, 'span[ng-show="showRating"]')
+    player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-primary')
+    recomended_player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-secondary')
+    play_time = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(2)')
+    weight = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(4)')
+    game_name = game_name.text
+    game_rating = game_rating.text
+    player_count=player_count.text
+    recomended_player_count=recomended_player_count.text
+    play_time = play_time.text
+    weight = weight.text
+    play_time = play_time.split('\n', 1)[0]
+    weight = weight.split('\n', 1)[0]
+    weight = weight.replace('Weight: ','')
+
 game_list = input('Enter games seperated by "|" >> ')
 game_list = (game_list .split('|'))
 browser=webdriver.Chrome()
@@ -20,42 +61,8 @@ for game in game_list:
     try:
         first_result = browser.find_element(By.CLASS_NAME, 'primary')
         first_result.click()
-        game_URL = browser.current_url
-        type == 'N/A'
-        if str(game_URL).find('https://boardgamegeek.com/boardgame/') != -1:
-            type = "Game"
-        if str(game_URL).find('https://boardgamegeek.com/boardgameexpansion/') != -1:
-            type = "Expansion"
-        game_name = browser.find_element(By.CSS_SELECTOR, 'h1>a[class="ng-binding"]')
-        try:
-            game_rank = browser.find_element(By.CSS_SELECTOR, 'a[class="rank-value ng-binding ng-scope"]')
-            game_rank = game_rank.text
-        except NoSuchElementException:
-            game_rank = 'N/A'
-        game_rating = browser.find_element(By.CSS_SELECTOR, 'span[ng-show="showRating"]')
-        player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-primary')
-        recomended_player_count = browser.find_element(By.CLASS_NAME, 'gameplay-item-secondary')
-        play_time = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(2)')
-        weight = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(4)')
-        game_name = game_name.text
-        game_rating = game_rating.text
-        player_count=player_count.text
-        recomended_player_count=recomended_player_count.text
-        play_time = play_time.text
-        weight = weight.text
-        play_time = play_time.split('\n', 1)[0]
-        weight = weight.split('\n', 1)[0]
-        weight = weight.replace('Weight: ','')
-        csv_file = Path('BoardGameGeek Game Data.csv')
-        if csv_file.exists():
-            with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
-                writer = csv.writer(save)
-                write_fields(writer)
-        else:
-            with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
-                writer = csv.writer(save)
-                writer.writerow(['Game Name', 'Type', 'Rating', 'Rank', 'Weight', 'Play Time', 'Player Count','Recommended Player Count', 'BGG URL'])               
-                write_fields(writer)
+        scrape_game_page()
+        add_to_csv()
     except NoSuchElementException:
          print(game + ' not found, skipped')
 
