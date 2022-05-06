5b78ff2be0d9740db779f2004d6c65ed47a182ef
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-06 14:28:42 -0700

    enable scraping expansions

--- BoardGameGeek.py
+++ BoardGameGeek.py
@@ -6,7 +6,7 @@ import csv
 from pathlib import Path
 
 def write_fields(x):
-    x.writerow([game_name, game_rating, game_rank, weight, play_time, player_count, recomended_player_count, game_URL])
+    x.writerow([game_name, type, game_rating, game_rank, weight, play_time, player_count, recomended_player_count, game_URL])
 
 game_list = input('Enter games seperated by "|" >> ')
 game_list = (game_list .split('|'))
@@ -21,9 +21,15 @@ for game in game_list:
         first_result = browser.find_element(By.CLASS_NAME, 'primary')
         first_result.click()
         game_URL = browser.current_url
+        type == 'N/A'
+        if str(game_URL).find('https://boardgamegeek.com/boardgame/') != -1:
+            type = "Game"
+        if str(game_URL).find('https://boardgamegeek.com/boardgameexpansion/') != -1:
+            type = "Expansion"
         game_name = browser.find_element(By.CSS_SELECTOR, 'h1>a[class="ng-binding"]')
         try:
             game_rank = browser.find_element(By.CSS_SELECTOR, 'a[class="rank-value ng-binding ng-scope"]')
+            game_rank = game_rank.text
         except NoSuchElementException:
             game_rank = 'N/A'
         game_rating = browser.find_element(By.CSS_SELECTOR, 'span[ng-show="showRating"]')
@@ -32,7 +38,6 @@ for game in game_list:
         play_time = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(2)')
         weight = browser.find_element(By.CLASS_NAME, 'gameplay-item:nth-child(4)')
         game_name = game_name.text
-        game_rank = game_rank.text
         game_rating = game_rating.text
         player_count=player_count.text
         recomended_player_count=recomended_player_count.text
@@ -49,7 +54,7 @@ for game in game_list:
         else:
             with open('BoardGameGeek Game Data.csv', 'a',newline='') as save:
                 writer = csv.writer(save)
-                writer.writerow(['Game Name', 'Rating', 'Rank', 'Weight', 'Play Time', 'Player Count','Recommended Player Count', 'BGG URL'])                
+                writer.writerow(['Game Name', 'Type', 'Rating', 'Rank', 'Weight', 'Play Time', 'Player Count','Recommended Player Count', 'BGG URL'])               
                 write_fields(writer)
     except NoSuchElementException:
          print(game + ' not found, skipped')
