2f58e9d455e0cdee07a5f12c37ac021b19c282ec
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-06 16:36:10 -0700

    Update BoardGameGeek Import From Collection.py

--- BoardGameGeek Import From Collection.py
+++ BoardGameGeek Import From Collection.py
@@ -12,24 +12,13 @@ browser.get('https://boardgamegeek.com/collection/user/' + username)
 
 game_links = []
 links = browser.find_elements(By.TAG_NAME, 'a.primary')
-
 for link in links:
     link = link.get_attribute("href")
     if str(link).find('https://boardgamegeek.com/boardgame/') != -1 or str(link).find('https://boardgamegeek.com/boardgameexpansion/') != -1:
         game_links.append(link)
+browser.quit
+
 for game in game_links:
     driver(game)
     scrape_game_page()
-
-    csv_file = Path(username + ' BoardGameGeek Collection.csv')
-    if csv_file.exists():
-        with open(username + ' BoardGameGeek Collection.csv', 'a',newline='') as save:
-            writer = csv.writer(save)
-            write_fields(writer)
-    else:
-        with open(username + ' BoardGameGeek Collection.csv', 'a',newline='') as save:
-            writer = csv.writer(save)
-            writer.writerow(['Game Name', 'Type', 'Rating', 'Rank', 'Weight', 'Play Time', 'Player Count','Recommended Player Count', 'BGG URL'])                
-            write_fields(writer)
-            
-browser.quit
+    add_to_csv(username + ' BoardGameGeek Collection.csv')
