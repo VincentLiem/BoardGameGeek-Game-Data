ec45f612545a980f1826db1b646bc0b12a59553d
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-06 23:44:25 -0700

    Update BoardGameGeek Import From Collection.py

--- BoardGameGeek Import From Collection.py
+++ BoardGameGeek Import From Collection.py
@@ -16,9 +16,10 @@ for link in links:
     link = link.get_attribute("href")
     if str(link).find('https://boardgamegeek.com/boardgame/') != -1 or str(link).find('https://boardgamegeek.com/boardgameexpansion/') != -1:
         game_links.append(link)
-browser.quit
 
 for game in game_links:
-    driver(game)
+    open_site(game)
     scrape_game_page()
     add_to_csv(username + ' BoardGameGeek Collection.csv')
+
+browser.quit
